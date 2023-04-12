90e2e150296ec9a977c3edf0265d881a7c424354
melquetrindade <melquetrindade654@gmail.com>
2023-04-12 11:03:00 -0300

    continuando desenvolvimento do projeto nubank. add: pageview

--- flutter_application_1/lib/proj_nubank/pages/home/widgets/my_app_bar.dart
+++ flutter_application_1/lib/proj_nubank/pages/home/widgets/my_app_bar.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
 class MyAppBar extends StatelessWidget {
+  final bool showMenu;
+  final VoidCallback onTap;
+
+  const MyAppBar({Key? key, this.showMenu = false, required this.onTap}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -8,24 +13,32 @@ class MyAppBar extends StatelessWidget {
         SizedBox(
           height: MediaQuery.of(context).padding.top,
         ),
-        Container(
-          color: Colors.blue,
-          height: 200,
-          child: Row(children: [
-            Image.network("assets/imgs/logo_3.png",
-              height: 35,
-              color: Colors.white,
+        GestureDetector(
+          onTap: onTap,
+          child: Container(
+            color: Colors.blue,
+            height: MediaQuery.of(context).size.height * 0.14,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Image.network(
+                    "assets/imgs/logo_3.png",
+                    height: 35,
+                    color: Colors.white,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "Melque",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  )
+                ]),
+                Icon(!showMenu ? Icons.expand_more : Icons.expand_less)
+              ],
             ),
-            SizedBox(
-              width: 10,
-            ),
-            Text("Melque", 
-              style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 18
-              ),
-            )
-          ]),
+          ),
         )
       ],
     );
