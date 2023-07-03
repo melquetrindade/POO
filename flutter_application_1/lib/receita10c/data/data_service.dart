69effffcf1b86e588bc663c7a37ca94dbcda0ff8
melquetrindade <melquetrindade654@gmail.com>
2023-07-03 20:42:07 -0300

    finalizando a receita 10c

--- flutter_application_1/lib/receita10c/data/data_service.dart
+++ flutter_application_1/lib/receita10c/data/data_service.dart
@@ -3,21 +3,37 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import '../util/ordenador.dart';
 
-enum TableStatus{idle,loading,ready,error}
-enum ItemType{
-  beer, coffee, nation, none;
+enum TableStatus { idle, loading, ready, error }
+
+//users (email; first_name; date_of_birth)
+//appliances (equipment; brand; uid)
+//addresses (city; street_name; community)
+
+enum ItemType {
+  users,
+  appliances,
+  addresses,
+  none;
+
   String get asString => '$name';
-  List<String> get columns => this == coffee? ["Nome", "Origem", "Tipo"] :
-                              this == beer? ["Nome", "Estilo", "IBU"]:
-                              this == nation? ["Nome", "Capital", "Idioma","Esporte"]:
-                              [] ;
-  List<String> get properties => this == coffee? ["blend_name","origin","variety"] :
-                              this == beer? ["name","style","ibu"]:
-                              this == nation? ["nationality","capital","language","national_sport"]:
-                              [] ;
+
+  List<String> get columns => this == users
+      ? ["E-mail", "Primeiro Nome", "Aniversário"]
+      : this == appliances
+          ? ["Equipamento", "Marca", "UID"]
+          : this == addresses
+              ? ["Cidade", "Rua", "Comunidade"]
+              : [];
+  List<String> get properties => this == users
+      ? ["email", "first_name", "date_of_birth"]
+      : this == appliances
+          ? ["equipment", "brand", "uid"]
+          : this == addresses
+              ? ["city", "street_name", "community"]
+              : [];
 }
 
-class DataService{
+class DataService {
   static const MAX_N_ITEMS = 15;
   static const MIN_N_ITEMS = 3;
   static const DEFAULT_N_ITEMS = 7;
@@ -29,63 +45,73 @@ class DataService{
     return _numberOfItems;
   }
 
-  set numberOfItems(n){
-    _numberOfItems = n < 0 ? MIN_N_ITEMS : n > MAX_N_ITEMS ? MAX_N_ITEMS : n;
+  set numberOfItems(n) {
+    _numberOfItems = n < 0
+        ? MIN_N_ITEMS
+        : n > MAX_N_ITEMS
+            ? MAX_N_ITEMS
+            : n;
   }
 
   List get possibleNItems {
     return _possibleNItems;
   }
 
-  set possibleNItems(n){
+  set possibleNItems(n) {
     _possibleNItems = possibleNItems;
   }
 
-  ValueNotifier<Map<String,dynamic>> tableStateNotifier = ValueNotifier({
-    'status':TableStatus.idle,
-    'dataObjects':[],
+  ValueNotifier<Map<String, dynamic>> tableStateNotifier = ValueNotifier({
+    'status': TableStatus.idle,
+    'dataObjects': [],
     'itemType': ItemType.none
   });
-  
-  void carregar(index){
-    final params = [ItemType.coffee, ItemType.beer, ItemType.nation];
-    carregarPorTipo(params[index]);  
+
+  void carregar(index) {
+    final params = [ItemType.users, ItemType.appliances, ItemType.addresses];
+    carregarPorTipo(params[index]);
   }
 
-  void ordenarEstadoAtual(final String propriedade){
-    List objetos =  tableStateNotifier.value['dataObjects'] ?? [];
+  void ordenarEstadoAtual(final String propriedade, final bool ordem) {
+    print(ordem);
+    List objetos = tableStateNotifier.value['dataObjects'] ?? [];
     if (objetos == []) return;
     Ordenador ord = Ordenador();
-    Decididor d = DecididorJSON(propriedade);
-    var objetosOrdenados = ord.ordenar(objetos, d.precisaTrocarAtualPeloProximo);
+    Decididor d = DecididorJSON(propriedade, ordem);
+    var objetosOrdenados =
+        ord.ordenar(objetos, d.precisaTrocarAtualPeloProximo);
 
-    emitirEstadoOrdenado(objetosOrdenados, propriedade);
+    emitirEstadoOrdenado(objetosOrdenados, propriedade, ordem);
   }
 
-  Uri montarUri(ItemType type){
+  Uri montarUri(ItemType type) {
+    print(type.asString);
+    print(_numberOfItems);
     return Uri(
-      scheme: 'https',
-      host: 'random-data-api.com',
-      path: 'api/${type.asString}/random_${type.asString}',
-      queryParameters: {'size': '$_numberOfItems'});
+        scheme: 'https',
+        host: 'random-data-api.com',
+        path: 'api/v2/${type.asString}',
+        queryParameters: {'size': '$_numberOfItems'}
+        );
   }
 
-  Future<List<dynamic>> acessarApi(Uri uri) async{
+  Future<List<dynamic>> acessarApi(Uri uri) async {
     var jsonString = await http.read(uri);
     var json = jsonDecode(jsonString);
     json = [...tableStateNotifier.value['dataObjects'], ...json];
     return json;
   }
 
-  void emitirEstadoOrdenado(List objetosOrdenados, String propriedade) {
-    var estado = Map<String, dynamic>.from(tableStateNotifier.value); 
+  void emitirEstadoOrdenado(
+      List objetosOrdenados, String propriedade, bool ordem) {
+    var estado = Map<String, dynamic>.from(tableStateNotifier.value);
     estado['dataObjects'] = objetosOrdenados;
     estado['sortCriteria'] = propriedade;
-    estado['ascending'] = true;
+    estado['ascending'] = !ordem;
     tableStateNotifier.value = estado;
   }
 
-  void emitirEstadoCarregando(ItemType type){
+  void emitirEstadoCarregando(ItemType type) {
     tableStateNotifier.value = {
       'status': TableStatus.loading,
       'dataObjects': [],
@@ -93,7 +119,7 @@ class DataService{
     };
   }
 
-  void emitirEstadoPronto(ItemType type, var json){
+  void emitirEstadoPronto(ItemType type, var json) {
     tableStateNotifier.value = {
       'itemType': type,
       'status': TableStatus.ready,
@@ -103,13 +129,15 @@ class DataService{
     };
   }
 
-  bool temRequisicaoEmCurso() => tableStateNotifier.value['status'] == TableStatus.loading;
-  bool mudouTipoDeItemRequisitado(ItemType type) => tableStateNotifier.value['itemType'] != type;
+  bool temRequisicaoEmCurso() =>
+      tableStateNotifier.value['status'] == TableStatus.loading;
+  bool mudouTipoDeItemRequisitado(ItemType type) =>
+      tableStateNotifier.value['itemType'] != type;
 
-  void carregarPorTipo(ItemType type) async{
+  void carregarPorTipo(ItemType type) async {
     //ignorar solicitação se uma requisição já estiver em curso
     if (temRequisicaoEmCurso()) return;
-    if (mudouTipoDeItemRequisitado(type)){
+    if (mudouTipoDeItemRequisitado(type)) {
       emitirEstadoCarregando(type);
     }
     var uri = montarUri(type);
@@ -120,19 +148,181 @@ class DataService{
 
 final dataService = DataService();
 
-class DecididorJSON extends Decididor{
+class DecididorJSON extends Decididor {
   final String propriedade;
   final bool crescente;
 
-  DecididorJSON(this.propriedade,[this.crescente = true]);
+  DecididorJSON(this.propriedade, this.crescente);
 
   @override
   bool precisaTrocarAtualPeloProximo(atual, proximo) {
     try {
-      final ordemCorreta = crescente ? [atual, proximo]: [proximo, atual] ;
-      return ordemCorreta[0][propriedade].compareTo(ordemCorreta[1][propriedade]) > 0;
-    } catch (error){
+      final ordemCorreta = crescente ? [atual, proximo] : [proximo, atual];
+      return ordemCorreta[0][propriedade]
+              .compareTo(ordemCorreta[1][propriedade]) >
+          0;
+    } catch (error) {
       return false;
-    }    
+    }
   }
 }
+
+/*
+enum TableStatus { idle, loading, ready, error }
+
+enum ItemType {
+  beer,
+  coffee,
+  nation,
+  none;
+
+  String get asString => '$name';
+
+  List<String> get columns => this == coffee
+      ? ["Nome", "Origem", "Tipo"]
+      : this == beer
+          ? ["Nome", "Estilo", "IBU"]
+          : this == nation
+              ? ["Nome", "Capital", "Idioma", "Esporte"]
+              : [];
+  List<String> get properties => this == coffee
+      ? ["blend_name", "origin", "variety"]
+      : this == beer
+          ? ["name", "style", "ibu"]
+          : this == nation
+              ? ["nationality", "capital", "language", "national_sport"]
+              : [];
+}
+
+class DataService {
+  static const MAX_N_ITEMS = 15;
+  static const MIN_N_ITEMS = 3;
+  static const DEFAULT_N_ITEMS = 7;
+
+  int _numberOfItems = DEFAULT_N_ITEMS;
+  List _possibleNItems = [MIN_N_ITEMS, DEFAULT_N_ITEMS, MAX_N_ITEMS];
+
+  int get numberOfItems {
+    return _numberOfItems;
+  }
+
+  set numberOfItems(n) {
+    _numberOfItems = n < 0
+        ? MIN_N_ITEMS
+        : n > MAX_N_ITEMS
+            ? MAX_N_ITEMS
+            : n;
+  }
+
+  List get possibleNItems {
+    return _possibleNItems;
+  }
+
+  set possibleNItems(n) {
+    _possibleNItems = possibleNItems;
+  }
+
+  ValueNotifier<Map<String, dynamic>> tableStateNotifier = ValueNotifier({
+    'status': TableStatus.idle,
+    'dataObjects': [],
+    'itemType': ItemType.none
+  });
+
+  void carregar(index) {
+    final params = [ItemType.coffee, ItemType.beer, ItemType.nation];
+    carregarPorTipo(params[index]);
+  }
+
+  void ordenarEstadoAtual(final String propriedade, final bool ordem) {
+    print(ordem);
+    List objetos = tableStateNotifier.value['dataObjects'] ?? [];
+    if (objetos == []) return;
+    Ordenador ord = Ordenador();
+    Decididor d = DecididorJSON(propriedade, ordem);
+    var objetosOrdenados =
+        ord.ordenar(objetos, d.precisaTrocarAtualPeloProximo);
+
+    emitirEstadoOrdenado(objetosOrdenados, propriedade, ordem);
+  }
+
+  //https://random-data-api.com/api/v2/users
+
+  Uri montarUri(ItemType type) {
+    return Uri(
+        scheme: 'https',
+        host: 'random-data-api.com',
+        path: 'api/${type.asString}/random_${type.asString}',
+        queryParameters: {'size': '$_numberOfItems'});
+  }
+
+  Future<List<dynamic>> acessarApi(Uri uri) async {
+    var jsonString = await http.read(uri);
+    var json = jsonDecode(jsonString);
+    json = [...tableStateNotifier.value['dataObjects'], ...json];
+    return json;
+  }
+
+  void emitirEstadoOrdenado(
+      List objetosOrdenados, String propriedade, bool ordem) {
+    var estado = Map<String, dynamic>.from(tableStateNotifier.value);
+    estado['dataObjects'] = objetosOrdenados;
+    estado['sortCriteria'] = propriedade;
+    estado['ascending'] = !ordem;
+    tableStateNotifier.value = estado;
+  }
+
+  void emitirEstadoCarregando(ItemType type) {
+    tableStateNotifier.value = {
+      'status': TableStatus.loading,
+      'dataObjects': [],
+      'itemType': type
+    };
+  }
+
+  void emitirEstadoPronto(ItemType type, var json) {
+    tableStateNotifier.value = {
+      'itemType': type,
+      'status': TableStatus.ready,
+      'dataObjects': json,
+      'propertyNames': type.properties,
+      'columnNames': type.columns
+    };
+  }
+
+  bool temRequisicaoEmCurso() =>
+      tableStateNotifier.value['status'] == TableStatus.loading;
+  bool mudouTipoDeItemRequisitado(ItemType type) =>
+      tableStateNotifier.value['itemType'] != type;
+
+  void carregarPorTipo(ItemType type) async {
+    //ignorar solicitação se uma requisição já estiver em curso
+    if (temRequisicaoEmCurso()) return;
+    if (mudouTipoDeItemRequisitado(type)) {
+      emitirEstadoCarregando(type);
+    }
+    var uri = montarUri(type);
+    var json = await acessarApi(uri);
+    emitirEstadoPronto(type, json);
+  }
+}
+
+final dataService = DataService();
+
+class DecididorJSON extends Decididor {
+  final String propriedade;
+  final bool crescente;
+
+  DecididorJSON(this.propriedade, this.crescente);
+
+  @override
+  bool precisaTrocarAtualPeloProximo(atual, proximo) {
+    try {
+      final ordemCorreta = crescente ? [atual, proximo] : [proximo, atual];
+      return ordemCorreta[0][propriedade]
+              .compareTo(ordemCorreta[1][propriedade]) >
+          0;
+    } catch (error) {
+      return false;
+    }
+  }
+}*/
